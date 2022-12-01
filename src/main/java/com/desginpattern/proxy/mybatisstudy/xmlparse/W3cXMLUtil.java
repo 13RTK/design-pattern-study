be73eebc74e3eafc85a9b1d750abf0ed7d9d9e27
alex <907785012@qq.com>
2022-12-01 16:44:57 +0800

    Add w3c dom test code

---
+++ src/main/java/com/desginpattern/proxy/mybatisstudy/xmlparse/W3cXMLUtil.java
@@ -0,0 +1,28 @@
+package com.desginpattern.proxy.mybatisstudy.xmlparse;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class W3cXMLUtil {
+    private static volatile DocumentBuilderFactory factory;
+
+    public static DocumentBuilder getDocumentBuilder() throws ParserConfigurationException {
+        return newInstance().newDocumentBuilder();
+    }
+
+    private W3cXMLUtil() {
+    }
+
+    public static DocumentBuilderFactory newInstance() {
+        if (factory == null) {
+            synchronized (W3cXMLUtil.class) {
+                if (factory == null) {
+                    factory = DocumentBuilderFactory.newInstance();
+                }
+            }
+        }
+
+        return factory;
+    }
+}
